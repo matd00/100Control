dec425d84e84e08a0e687b15ed9820b0a6e00b19
matd00 <matdantandas@gmail.com>
2026-03-09 17:02:45 -0300

    fix: Adiciona converters JSON flexiveis para API SuperFrete

--- Integrations/SuperFrete/Models/SuperFreteApiModels.cs
+++ Integrations/SuperFrete/Models/SuperFreteApiModels.cs
@@ -1,6 +1,107 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Integrations.SuperFrete.Models;
+
+#region JSON Converters
+
+/// <summary>
+/// Converter que aceita string, número ou null e converte para string
+/// </summary>
+public class FlexibleStringConverter : JsonConverter<string?>
+{
+    public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.String:
+                return reader.GetString();
+            case JsonTokenType.Number:
+                if (reader.TryGetInt64(out long longValue))
+                    return longValue.ToString();
+                if (reader.TryGetDecimal(out decimal decimalValue))
+                    return decimalValue.ToString();
+                return reader.GetDouble().ToString();
+            case JsonTokenType.True:
+                return "true";
+            case JsonTokenType.False:
+                return "false";
+            case JsonTokenType.Null:
+                return null;
+            default:
+                // Para objetos ou arrays, retorna null
+                reader.Skip();
+                return null;
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, string? value, JsonSerializerOptions options)
+    {
+        if (value == null)
+            writer.WriteNullValue();
+        else
+            writer.WriteStringValue(value);
+    }
+}
+
+/// <summary>
+/// Converter que aceita int ou string e converte para int
+/// </summary>
+public class FlexibleIntConverter : JsonConverter<int>
+{
+    public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.Number:
+                return reader.GetInt32();
+            case JsonTokenType.String:
+                var str = reader.GetString();
+                if (int.TryParse(str, out int result))
+                    return result;
+                return 0;
+            default:
+                reader.Skip();
+                return 0;
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+    {
+        writer.WriteNumberValue(value);
+    }
+}
+
+/// <summary>
+/// Converter que aceita decimal ou string e converte para decimal
+/// </summary>
+public class FlexibleDecimalConverter : JsonConverter<decimal>
+{
+    public override decimal Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.Number:
+                return reader.GetDecimal();
+            case JsonTokenType.String:
+                var str = reader.GetString();
+                if (decimal.TryParse(str, System.Globalization.NumberStyles.Any, 
+                    System.Globalization.CultureInfo.InvariantCulture, out decimal result))
+                    return result;
+                return 0;
+            default:
+                reader.Skip();
+                return 0;
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, decimal value, JsonSerializerOptions options)
+    {
+        writer.WriteNumberValue(value);
+    }
+}
+
+#endregion
 
 #region Request Models
 
@@ -86,48 +187,59 @@ public class SuperFreteInvoice
 public class SuperFreteQuoteResponse
 {
     [JsonPropertyName("id")]
+    [JsonConverter(typeof(FlexibleStringConverter))]
     public string? Id { get; set; }
 
     [JsonPropertyName("price")]
+    [JsonConverter(typeof(FlexibleDecimalConverter))]
     public decimal Price { get; set; }
 
     [JsonPropertyName("discount")]
+    [JsonConverter(typeof(FlexibleDecimalConverter))]
     public decimal Discount { get; set; }
 
     [JsonPropertyName("delivery_time")]
+    [JsonConverter(typeof(FlexibleIntConverter))]
     public int DeliveryTime { get; set; }
 
     [JsonPropertyName("delivery_range")]
     public SuperFreteDeliveryRange? DeliveryRange { get; set; }
 
     [JsonPropertyName("name")]
+    [JsonConverter(typeof(FlexibleStringConverter))]
     public string? Name { get; set; }
 
     [JsonPropertyName("company")]
     public SuperFreteCompany? Company { get; set; }
 
     [JsonPropertyName("error")]
+    [JsonConverter(typeof(FlexibleStringConverter))]
     public string? Error { get; set; }
 }
 
 public class SuperFreteDeliveryRange
 {
     [JsonPropertyName("min")]
+    [JsonConverter(typeof(FlexibleIntConverter))]
     public int Min { get; set; }
 
     [JsonPropertyName("max")]
+    [JsonConverter(typeof(FlexibleIntConverter))]
     public int Max { get; set; }
 }
 
 public class SuperFreteCompany
 {
     [JsonPropertyName("id")]
+    [JsonConverter(typeof(FlexibleIntConverter))]
     public int Id { get; set; }
 
     [JsonPropertyName("name")]
+    [JsonConverter(typeof(FlexibleStringConverter))]
     public string? Name { get; set; }
 
     [JsonPropertyName("picture")]
+    [JsonConverter(typeof(FlexibleStringConverter))]
     public string? Picture { get; set; }
 }
 
